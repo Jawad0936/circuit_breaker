276acd4ed7a28bdf49aec03cbf4eb9704b269e9f
Jawad Ul Hassan <jawad.ul.hassan2001@gmail.com>
2026-07-01 01:59:22 +0500

    Initialising the project

---
+++ lib/circuit_breaker.ex
@@ -0,0 +1,18 @@
+defmodule CircuitBreaker do
+  @moduledoc """
+  Documentation for `CircuitBreaker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CircuitBreaker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
